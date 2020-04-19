b8323da84a7044fe3f9e10a8019b1a2c35a2d3a4
dorotamejlun <63667455+dorotamejlun@users.noreply.github.com>
2020-04-19 23:11:44 +0200

    Update wd_cw04 NaZakupy.py

--- wd_cw04 NaZakupy.py
+++ wd_cw04 NaZakupy.py
@@ -1,23 +1,6 @@
 import sys
 print("\nzad4")
-"""
-Stwórz klasę NaZakupy, która będzie przechowywać atrybuty:
 
-nazwa_produktu, ilosc, jednostka_miary, cena_jed 
-oraz metody:
-
-konstruktor – który nadaje wartości
-
-wyświetl_produkt() – drukuje informacje o produkcie 
-na ekranie
-
-ile_produktu() – informacje ile danego produktu 
-ma być czyli ilosc + jednostka_miary np. 1 szt., 3 kg itd.
-
-ile_kosztuje() – oblicza ile kosztuje dana ilość produktu 
-np. 3 kg ziemniaków a cena_jed wynosi 2 zł/kg wówczas 
-funkcja powinna zwrócić wartość 3*2
-"""
 class NaZakupy:
     def __init__(self, nazwa_produktu, ilosc, jednostka_miary, cena_jed):
         self.nazwa_produktu = nazwa_produktu
