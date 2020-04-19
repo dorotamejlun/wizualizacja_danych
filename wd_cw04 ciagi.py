dc6519db57a8b54978b885ab799b4210dbdd0786
dorotamejlun <63667455+dorotamejlun@users.noreply.github.com>
2020-04-20 01:14:17 +0200

    Add files via upload

---
+++ wd_cw04 ciagi.py
@@ -0,0 +1,14 @@
+print("\nzad5")
+import sys
+
+class ciagi:
+    a1 = int(input('Podaj a1: '))
+    n = int(input("Podaj n: "))
+    r = int(input("Podaj r: "))
+    def suma(self):
+        print((self.a1+self.a1+(self.n-1)*self.r)/2)
+obiekt = ciagi()
+print(obiekt.suma())
+
+
+
