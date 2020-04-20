5cff287b34b09ae7e264bb14c232c955b58a8229
dorotamejlun <63667455+dorotamejlun@users.noreply.github.com>
2020-04-20 17:47:36 +0200

    Update wd_cw04 ciagi.py

--- wd_cw04 ciagi.py
+++ wd_cw04 ciagi.py
@@ -1,11 +1,20 @@
 print("\nzad5")
-import sys
 
-class ciagi:
+class Ciagi:
     def __init__(self, a,n,r):
         self.a = a
         self.n = n
         self.r = r
+    def wyswietl_dane(self):
+        return (self.a,self.n,self.r)
+    def wyswietl_wyrazy(self):
+        for i in range (self.n):
+           print(self.a+i*self.r)
 
     def suma(self):
-        print (((self.a+self.a+(self.n-1)*self.r)/2))
+        return(((2*self.a+(self.n-1)*self.r)/2) *self.n)
+
+ciag1 = Ciagi(1,5,2)
+print("wyraz1:",ciag1.a,"liczba wyrazow:",ciag1.n,"różnica:",ciag1.r)
+print("suma:",ciag1.suma())
+ciag1.wyswietl_wyrazy()
