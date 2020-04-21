5c0bc1796b62b3308eb77f9df7c73a558ef27b1a
dorotamejlun <63667455+dorotamejlun@users.noreply.github.com>
2020-04-21 20:39:13 +0200

    Update wd_cw04 slowa.py

--- wd_cw04 slowa.py
+++ wd_cw04 slowa.py
@@ -2,20 +2,40 @@
 print("zad6")
 
 class Slowa:
+    
     def __init__(self, wyraz1,wyraz2):
         self.wyraz1 = wyraz1
         self.wyraz2 = wyraz2
+    
     def spr_czy_palindrom(self):
         r = self.wyraz1[::-1]
         if (self.wyraz1 == r):
             return "Wyraz jest palindromem."
         else:
             return "Wyraz nie jest palindromem."
+    
     def spr_czy_metagramy(self):
-            
+        ile=0
+        for i in range (len(self.wyraz1)):
+            if (self.wyraz1[i] != self.wyraz2[i]):
+                ile +=1
+        if (ile == 1):
+            return "Metagramy"
+        else:
+            return "Nie metagramy"
+    
+    def spr_czy_anagramy(self):
+        if (sorted(self.wyraz1)==sorted(self.wyraz2)):
+            return "Anagramy"
+        else:
+            return "Nie anagramy"
+    
+    def wyswietl(self):
+        return self.wyraz1, self.wyraz2
 
-
-slowo1 = Slowa("def","def")
+slowo1 = Slowa("kajak","akkaj")
 
 print(slowo1.spr_czy_palindrom())
 print(slowo1.spr_czy_metagramy())
+print(slowo1.spr_czy_anagramy())
+print(slowo1.wyswietl())
